0b43de4b15656e8d27bacadbea0c10a9c5a964ff
Joseph Viviano <joseph@viviano.ca>
2018-11-14 13:23:43 -0500

    added bits for transforming between int and string labels

--- train.py
+++ train.py
@@ -1,27 +1,40 @@
+#!/usr/bin/env python
 """
 imports a set of experiments from experiments.py, runs them, and write results
 """
-import os
-import logging
 from experiments import experiment01
+from utils import load_data, show_example, get_y_map, convert_y
+import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 # adds a simple logger
 logging.basicConfig(level=logging.INFO, format="[%(name)s:%(funcName)s:%(lineno)s] %(levelname)s: %(message)s")
 LOGGER = logging.getLogger(os.path.basename(__file__))
 
+category_map = {}
 
 def main():
 
-    log_fname = "experiment01_log.txt"
+    log_fname = "logs/experiment01_log.txt"
     if os.path.isfile(log_fname):
         os.remove(log_fname)
     log_hdl = logging.FileHandler(log_fname)
     log_hdl.setFormatter(logging.Formatter('%(message)s'))
     LOGGER.addHandler(log_hdl)
 
-    # results = experiment01
+    data = load_data()
+
+    # way to map between string labels and int labels
+    y_map = get_y_map(data)
+    data['y']['train'] = convert_y(data['y']['train'], y_map)
+    data['y']['valid'] = convert_y(data['y']['valid'], y_map)
+
+
+    # results = experiment02
     # write results
-    pass
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,70 @@
+"""
+helper functions
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+def show_example(image):
+    """reshapes image to 100x100, plots)"""
+    plt.imshow(image.reshape(100,100))
+    plt.show()
+
+
+def get_y_map(data):
+    """gets all the unique values in y to allow str <-> int conversion"""
+    y_map = LabelEncoder()
+    y_map.fit(data['y']['train'])
+    return(y_map)
+
+
+def convert_y(y, y_map):
+    """converts all y in data to int if str, else str if int"""
+    # convert integers to string labels
+    if np.issubdtype(type(y[0]), np.number):
+        return(y_map.inverse_transform(y))
+
+    # convert string labels to integers
+    else:
+        return(y_map.transform(y))
+
+
+def load_data(valid_pct=0.1):
+    """loads the data into a structure"""
+    X_train = np.load('data/train_images.npy', encoding='latin1')
+    X_test  = np.load('data/test_images.npy', encoding='latin1')
+    y_train = np.genfromtxt('data/train_labels.csv', names=True, delimiter=',',
+        dtype=[('Id', 'i8'), ('Category', 'S5')])
+
+    # get data into numpy matrices
+    X_train_output = []
+    for i in range(len(X_train)):
+       X_train_output.append(X_train[i, 1])
+    X_train = np.vstack(X_train_output)
+
+    X_test_output = []
+    for i in range(len(X_test)):
+       X_test_output.append(X_test[i, 1])
+    X_test = np.vstack(X_test_output)
+
+    y_train_output = []
+    for i in range(len(y_train)):
+        y_train_output.append(np.array(y_train[i][1]).astype(np.str))
+    y_train = np.hstack(y_train_output)
+
+    # make validation set
+    n_valid = int(np.floor(valid_pct * len(X_train)))
+
+    X_valid = X_train[:n_valid, :]
+    X_train = X_train[n_valid:, :]
+    y_valid = y_train[:n_valid]
+    y_train = y_train[n_valid:]
+
+    # data is accessed as data['X']['valid']
+    data = {'X': {'train': X_train, 'valid': X_valid, 'test': X_test},
+            'y': {'train': y_train, 'valid': y_valid}
+    }
+
+    return(data)
+
+
