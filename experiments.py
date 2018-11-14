590da1142f4c0542b0f4b51aaeac49eda465c603
Joseph Viviano <joseph@viviano.ca>
2018-11-14 14:58:39 -0500

    python 3 style changes

--- experiments.py
+++ experiments.py
@@ -7,19 +7,24 @@ def experiment01():
     """imagenet"""
     pass
 
+
 def experiment02():
     """imagenet + PrELU"""
     pass
+
 
 def experiment03():
     """adaboost logistic regression"""
     pass
 
+
 def experiment04():
     """cnn and random forest ensemble"""
     pass
 
+
 def experiment05(data):
     """Base line: SVM (without Kernel)"""
-    return SVM(data)
+    return(SVM(data))
+
 
--- models.py
+++ models.py
@@ -1,16 +1,22 @@
 """
 holds our models (e.g., imagenet, cnns, etc, to be imported into experiments.py)
 """
+
+from sklearn.svm import SVC
+
+
 def SVM(data):
     """ Base line: Linear classifier (without kernel)"""
-    from sklearn.svm import SVC  
-    svclassifier = SVC(kernel='linear')  
-    svclassifier.fit(data['X']['train'], data['y']['train']) 
-    
-    y_pred = svclassifier.predict(data['X']['valid'])  
-    
-    return y_pred
-    
+    svclassifier = SVC(kernel='linear')
+    svclassifier.fit(data['X']['train'], data['y']['train'])
+
+    y_pred = svclassifier.predict(data['X']['valid'])
+
+    return(y_pred)
+
 
 def logistic_regression(data):
     """Base line: Linear classifier"""
+    pass
+
+
