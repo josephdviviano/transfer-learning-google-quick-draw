b5da82f56a89a3d0bc4a29259d181631c52e5cfb
Joseph Viviano <vivianoj@leto38.iro.umontreal.ca>
2018-11-23 20:05:57 -0500

    more figures

--- plot_stuff.py
+++ plot_stuff.py
@@ -28,9 +28,11 @@ from sklearn.metrics import confusion_matrix
 
 mat = confusion_matrix(performance['best_preds'], performance['best_reals'])
 label = ['apple', 'empty', 'moust', 'mouth', 'mug', 'nail', 'nose', 'octag', 'paint', 'panda', 'parro', 'peanu', 'pear', 'pencil', 'pengu', 'pillo', 'pinea', 'pool', 'rabbi', 'rhino', 'rifle', 'rolle', 'sailb', 'scorp', 'screw', 'shove', 'sink', 'skate', 'skull', 'spoon']
-plot = sns.heatmap(mat, annot=False, linewidths=0.5, cmap='Blues')
-plot.set_xticklabels(label, rotation=45)
-plot.set_yticklabels(label, rotation=45)
+plot = sns.heatmap(mat, annot=False, linewidths=0.5, cmap='Blues', xticklabels=label, yticklabels=label)
+#plot.set_xticks(np.arange(len(label)), minor=True)
+#plot.set_yticks(np.arange(len(label)), minor=True)
+#plot.set_xticklabels(label, rotation=45, minor=True)
+#plot.set_yticklabels(label, rotation=45, minor=True)
 plot.set_xlabel('Actual class')
 plot.set_ylabel('Predicted class')
 plot.set_title('Confusion Matrix for Resnet')
